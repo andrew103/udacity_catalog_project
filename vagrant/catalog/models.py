57b0ba4ac19972c7a2828a4b67486d6f1e057496
Andrew Euredjian <andrew_103@rocketmail.com>
2017-07-05 18:22:56 -0700

    models.py finished (i think/hope)

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
@@ -10,13 +10,65 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False)
+    email = Column(String(200))
+    picture = Column(String(250))
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    @property
+    def serialize(self):
+        return {
+            'id' = self.id
+            'name' = self.name
+            'email' = self.email
+            'picture' = self.picture
+            'hash' = self.password_hash
+        }
+
 
 class Category(Base):
     __tablename__ = 'category'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'id' = self.id
+            'name' = self.name
+            'creator' = self.user_id
+        }
+
 
 class Item(Base):
     __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    cat_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        return {
+            'id' = self.id
+            'name' = self.name
+            'creator' = self.user_id
+            'category' = self.cat_id
+        }
+
 
 
 engine = create_engine('sqlite:///usersWithOAuth.db')
